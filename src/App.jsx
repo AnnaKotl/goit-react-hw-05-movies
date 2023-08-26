5bc1e24290c27f75606223142631e687db0760ad
Анна Котляр <kotlyaranya1771@gmail.com>
2023-08-26 14:12:15 +0300

    main

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+
+
+
+export const App = () => {
+
+};
+
+
+// Imports
+// import { SharedLayout } from "path/to/components/SharedLayout";
+
+// export const App = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<SharedLayout />}>
+//         <Route index element={<Home />} />
+//         <Route path="about" element={<About />}>
+//           <Route path="mission" element={<Mission />} />
+//           <Route path="team" element={<Team />} />
+//           <Route path="reviews" element={<Reviews />} />
+//         </Route>
+//         <Route path="products" element={<Products />} />
+//         <Route path="products/:productId" element={<ProductDetails />} />
+//       </Route>
+//     </Routes>
+//   );
+// };
